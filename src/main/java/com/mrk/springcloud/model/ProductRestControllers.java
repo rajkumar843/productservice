02c8af79900fac987d91949cd64483cf59bf90b8
rajkumar834 <raj.yadavs1124@gmail.com>
2026-05-26 08:52:12 +0530

    Added Slf4j logger

--- src/main/java/com/mrk/springcloud/model/ProductRestControllers.java
+++ src/main/java/com/mrk/springcloud/model/ProductRestControllers.java
@@ -2,6 +2,9 @@ package com.mrk.springcloud.model;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,22 +20,25 @@ import com.mrk.springcloud.repos.ProductRepo;
 @RequestMapping("/productapi")
 @Tag(name = "Product APIs", description = "Product Microservice APIs")
 public class ProductRestControllers {
-	
-	@Autowired
-    private	ProductRepo repo;
-	@Autowired
-	private RestTemplate restTemplet;
-	
-	@Value("${couponservice.url}")
-	private String couponServiceURL;
-	
-	
-	@RequestMapping(value = "/products",method = RequestMethod.POST)
+    private static final Logger logger = LoggerFactory.getLogger(ProductRestControllers.class);
+
+    @Autowired
+    private ProductRepo repo;
+    @Autowired
+    private RestTemplate restTemplet;
+
+    @Value("${couponservice.url}")
+    private String couponServiceURL;
+
+
+    @RequestMapping(value = "/products", method = RequestMethod.POST)
     @Operation(summary = "Create products ")
-	public Product create(@RequestBody Product product ) {
-		 Coupon cupon  =restTemplet.getForObject(couponServiceURL+product.getCouponCode(), Coupon.class);
-		 product.setPrice(product.getPrice().subtract(cupon.getDiscount()));
-		return repo.save(product);
-	}
+    public Product create(@RequestBody Product product) {
+        logger.info("we are in create method {}");
+        Coupon cupon = restTemplet.getForObject(couponServiceURL + product.getCouponCode(), Coupon.class);
+        product.setPrice(product.getPrice().subtract(cupon.getDiscount()));
+        logger.info("we are getting product" + product);
+        return repo.save(product);
+    }
 
 }
